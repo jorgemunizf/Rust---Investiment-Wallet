025a4d50bbe9d9b60eeaf8a0fab74de92b5cf2ae
Jorge Filho <jorge.mfilho6@gmail.com>
2026-08-13 18:28:16 -0300

    feat: implement user creation function and update dependencies

--- src/users/repository.rs
+++ src/users/repository.rs
@@ -1,4 +1,5 @@
 use sqlx::PgPool;
+use uuid::Uuid;
 
 use super::model::User;
 
@@ -18,6 +19,42 @@ pub async fn find_by_email(pool: &PgPool, email: &str) -> Result<Option<User>, s
         email
     )
     .fetch_optional(pool)
+    .await?;
+
+    Ok(user)
+}
+
+pub async fn create_user(
+    pool: &PgPool,
+    name: &str,
+    email: &str,
+    password_hash: &str,
+) -> Result<User, sqlx::Error> {
+    let id = Uuid::new_v4();
+
+    let user = sqlx::query_as!(
+        User,
+        r#"
+        INSERT INTO users (
+            id,
+            name,
+            email,
+            password_hash
+        )
+        VALUES ($1,$2,$3,$4)
+        RETURNING
+            id,
+            name,
+            email,
+            password_hash,
+            created_at
+        "#,
+        id,
+        name,
+        email,
+        password_hash
+    )
+    .fetch_one(pool)
     .await?;
 
     Ok(user)
